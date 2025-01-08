5d7ea362f226d91af4c1da0cd4e3e54eff65e599
DAGSSSKIE <michaelangelobuganan6@gmail.com>
2025-01-08 20:41:58 +0800

    Create Activity 1.js

---
+++ Activity 1.js
@@ -0,0 +1,9 @@
+let name = "MICHAEL ANGELO BUGANAN"; 
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "MALE"; 
+let twitterHandle = "@YourTwitterHandle";
+
+console.log("NAME:", MICHAEL ANGELO BUGANAN);
+console.log("Ambition:",PROGRAMMER);
+console.log("Gender:", MALE);
+console.log("Twitter Handle:", twitterHandle);
